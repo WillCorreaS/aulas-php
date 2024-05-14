1e876f722bf8569fa23bf85850975c4339a91695
Wilian F. Correa Souza <wilian.silva3@fatec.sp.gov.br>
2024-05-14 17:28:36 -0300

    fim da primeira aula

---
+++ 01-hello-world.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula 01 - PHP</title>
+    <style>
+        
+    </style>
+</head>
+<body>
+    <h1>Primeiros passos com <b>PHP</b>.</h1>
+    <hr>
+    
+    <!-- Para adicionar PHP na página é necessário utilizar os DELIMITADORES (tag) php "< ?php --conteudo -- ?>" -->
+    <?php 
+    //Comando de saída
+    echo "Olá ";
+    echo "Senac Penha";
+
+    echo "<p style='text-aling:center'>Senac Penha</p>";
+    echo '<p style="text-aling:center">Senac Penha</p>';
+    echo "<p style=\'text-aling:center\'>Senac Penha</p>";
+    ?>
+    
+</body>
+</html>
