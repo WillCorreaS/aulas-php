ca9f3165d7e3f6a1226afd4ca737368ea4102d68
Wilian F. Correa Souza <wilian.silva3@fatec.sp.gov.br>
2024-05-15 16:02:14 -0300

    Create 03-arrays.php

---
+++ 03-arrays.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Arrays</title>
+</head>
+<body>
+<h1>Trabalhando com Arrays</h1>
+<hr>
+<p>Array são dados indexados na memório que podem ser acessados utilizando o indice em que está alocado dentro do array</p>
+<hr>
+<h2></h2>
+
+
+
+    
+</body>
+</html>
