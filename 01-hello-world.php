5da90638d1666375325f6d61c0cb9d440ed39cba
Wilian F. Correa Souza <wilian.silva3@fatec.sp.gov.br>
2024-05-15 13:53:11 -0300

    Update 01-hello-world.php

--- 01-hello-world.php
+++ 01-hello-world.php
@@ -4,8 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Aula 01 - PHP</title>
+
+    <!-- utilizando bootstrap CSS-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
     <style>
-        
+        p {
+            
+        }
     </style>
 </head>
 <body>
@@ -23,5 +29,9 @@
     echo "<p style=\'text-aling:center\'>Senac Penha</p>";
     ?>
     
+
+<!-- utilizando bootstrap Javascrips-->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
 </body>
 </html>
