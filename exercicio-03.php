88ce663b48105c52835c5344ee6d8eb40c888d44
Wilian F. Correa Souza <wilian.silva3@fatec.sp.gov.br>
2024-05-16 17:22:00 -0300

    Update exercicio-03.php

--- exercicio-03.php
+++ exercicio-03.php
@@ -4,8 +4,31 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Exercício 03</title>
+        <!-- utilizando bootstrap CSS-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+<style>
+    body {
+        background-color: lightblue;
+    }
+    </style>
+
 </head>
 <body>
+    <h1>Exercício 03 <b>PHP</b></h1>
+    <hr>
+    <div class="container">
+<?php
+$meses = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
+
+foreach($meses as $mes){?>
+    <ol>
+        <li><?=$mes?></li>
+    </ol>
+<?php
+}
+?>
+    </div>
     
 </body>
 </html>
